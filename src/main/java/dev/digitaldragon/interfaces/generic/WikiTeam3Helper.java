f417b59f681cb96d02d6174124ed3e17fbc0eee1
DigitalDragon <34419970+DigitalDwagon@users.noreply.github.com>
2023-09-25 22:05:18 -0400

    use new wikiteam3 parser (JCommander)

--- src/main/java/dev/digitaldragon/interfaces/generic/WikiTeam3Helper.java
+++ src/main/java/dev/digitaldragon/interfaces/generic/WikiTeam3Helper.java
@@ -1,10 +1,13 @@
 package dev.digitaldragon.interfaces.generic;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
 import dev.digitaldragon.WikiBot;
 import dev.digitaldragon.archive.WikiTeam3Plugin;
 import dev.digitaldragon.interfaces.UserErrorException;
 import dev.digitaldragon.jobs.Job;
 import dev.digitaldragon.jobs.JobManager;
+import dev.digitaldragon.jobs.wikiteam.WikiTeam3Args;
 import dev.digitaldragon.jobs.wikiteam.WikiTeam3Job;
 import dev.digitaldragon.parser.CommandLineParser;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -14,42 +17,35 @@ import java.util.UUID;
 public class WikiTeam3Helper {
     //TODO switch to the new Args class-type parser here to enable compatibility with more platforms and modules.
     public static String beginJob(String unparsedArgs, String userName) throws UserErrorException {
-        CommandLineParser parser = WikiTeam3Plugin.getCommandLineParser();
-        parser.addBooleanOption("old-backend");
+        WikiTeam3Args args = new WikiTeam3Args();
         try {
-            parser.parse(unparsedArgs.split(" "));
-        } catch (IllegalArgumentException e) {
-            return e.getMessage();
+            JCommander.newBuilder()
+                    .addObject(args)
+                    .build()
+                    .parse(unparsedArgs.split(" (?=([^\"]*\"[^\"]*\")*[^\"]*$)"));
+        } catch (ParameterException e) {
+            return "Invalid parameters or options! Hint: make sure that your --explain is in quotes if it has more than one word. (-e \"no coverage\")";
         }
-
-        if (parser.getOption("url") == null && parser.getOption("api") == null && parser.getOption("index") == null) {
-            return "You need to specify --url, --api, or --index! Note: URLs are required in the form of an option, eg \"--url https://wikipedia.org\"";
-        }
-
-        if (parser.getOption("explain") == null)
-            return "An explanation is required! Note: A new bot update now requires explanations in the form of an option, eg \"--explain Closing soon\"";
-
-
-        return beginJob(parser, userName);
+        return beginJob(args, userName);
     }
 
-    public static String beginJob(CommandLineParser parser, String userName) throws UserErrorException {
-        TextChannel discordChannel = WikiBot.getLogsChannelSafely();
+    public static String beginJob(WikiTeam3Args args, String userName) throws UserErrorException {
+        args.check();
+        if (args.getUrl() == null && args.getApi() == null && args.getIndex() == null)
+            return "You need to specify --url, --api, or --index! Note: URLs are required in the form of an option, eg \"--url https://wikipedia.org\"";
+        if (args.getExplain() == null)
+            return "An explanation is required! Note: Explanations are required in the form of an option, eg \"--explain Closing soon\"";
 
-        String jobName = parser.getOption("url") == null ? null : parser.getOption("url").toString();
+        String jobName = args.getUrl();
         if (jobName == null)
-            jobName = parser.getOption("api") == null ? null : parser.getOption("api").toString();
+            jobName = args.getApi();
         if (jobName == null)
-            jobName = parser.getOption("index") == null ? null : parser.getOption("index").toString();
+            jobName = args.getIndex();
 
-        String explain = parser.getOption("explain").toString();
+        String explain = args.getExplain();
 
-        if (parser.getOption("old-backend") == Boolean.TRUE) {
-            WikiTeam3Plugin.startJob(discordChannel, explain, userName, userName, WikiTeam3Plugin.parserToOptions(parser));
-        } else {
-            Job job = new WikiTeam3Job(userName, UUID.randomUUID().toString(), userName, WikiTeam3Plugin.parserToOptions(parser), explain);
-            JobManager.submit(job);
-        }
+        Job job = new WikiTeam3Job(userName, UUID.randomUUID().toString(), jobName, args.get(), explain);
+        JobManager.submit(job);
         return null;
     }
 }
