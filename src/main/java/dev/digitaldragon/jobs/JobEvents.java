ae501b633a84739367a283dc1410b07e770d9fa3
DigitalDragon <34419970+DigitalDwagon@users.noreply.github.com>
2023-11-19 19:45:48 -0500

    clean commented out code from old JobEvents class

--- src/main/java/dev/digitaldragon/jobs/JobEvents.java
+++ src/main/java/dev/digitaldragon/jobs/JobEvents.java
@@ -18,24 +18,8 @@ public class JobEvents {
      * @param job The job that has failed.
      */
     public static void onJobFailure(Job job) { //This method is called when a job fails (due to an improper task exit code, etc, as dictated by the job). The runningTask is the task that failed
-        //UpdatesWebsocket.sendLogMessageToClients(job, "FAILED");
-
-
-        /*IRCClient.sendMessage(job.getUserName(), "Job " + job.getId() + " failed with exit code " + job.getFailedTaskCode() + ".");
-        IRCClient.sendMessage("Explanation: " + job.getExplanation());
-        IRCClient.sendMessage("Logs URL: " + job.getLogsUrl());*/
-
         JobFailureEvent event = new JobFailureEvent(job);
         WikiBot.getBus().post(event);
-
-        /*TextChannel failChannel = WikiBot.getInstance().getTextChannelById(EnvConfig.getConfigs().get("discord_failure_channel"));
-        if (failChannel != null)
-            failChannel.sendMessage(String.format("%s for %s:\n\nThread: %s\nLogs: %s\nJob ID: `%s`\nFailed Task: %s\nExit code: `%s`\nExplanation: ```%s```", job.getName(), job.getUserName(), job.getThreadChannel(), job.getLogsUrl(), job.getId(), job.getRunningTask(), job.getFailedTaskCode(), job.getExplanation())).queue();
-
-        job.getThreadChannel().sendMessage("Job " + job.getId() + " failed with exit code " + job.getFailedTaskCode() + ".").queue();
-        job.getThreadChannel().sendMessage("Explanation: ```" + job.getExplanation() + "```").queue();
-        job.getThreadChannel().sendMessage("Logs are available at " + job.getLogsUrl()).queue();
-        job.getThreadChannel().sendMessage("Task indicated as failed.").queue();*/
     }
 
     /**
@@ -44,23 +28,8 @@ public class JobEvents {
      * @param job The job that has succeeded.
      */
     public static void onJobSuccess(Job job) { //This method is called when a job succeeds.
-        //UpdatesWebsocket.sendLogMessageToClients(job, "SUCCESS");
-
-        /*IRCClient.sendMessage(job.getUserName(), "Success! Job " + job.getId() + " completed successfully.");
-        IRCClient.sendMessage("Archive URL: " + job.getArchiveUrl());
-        IRCClient.sendMessage("Explanation: " + job.getExplanation());
-        IRCClient.sendMessage("Logs URL: " + job.getLogsUrl());*/
-
         JobSuccessEvent event = new JobSuccessEvent(job);
         WikiBot.getBus().post(event);
-
-        /*TextChannel successChannel = WikiBot.getInstance().getTextChannelById(EnvConfig.getConfigs().get("discord_success_channel"));
-        if (successChannel != null)
-            successChannel.sendMessage(String.format("%s for %s:\n\nThread: %s\nLogs: %s\nJob ID: `%s`\nArchive URL: %s\nExplanation: ```%s```", job.getName(), job.getUserName(), job.getThreadChannel(), job.getLogsUrl(), job.getId(), job.getArchiveUrl(), job.getExplanation())).queue();
-
-        job.getThreadChannel().sendMessage("Job ended.").queue();
-        job.getThreadChannel().sendMessage("Explanation: ```" + job.getExplanation() + "```").queue();
-        job.getThreadChannel().sendMessage("Logs are available at " + job.getLogsUrl()).queue();*/
     }
 
     /**
@@ -68,17 +37,9 @@ public class JobEvents {
      *
      * @param job The job that was aborted.
      */
-    public static void onJobAbort(Job job) { //This method is called when a job fails because it was aborted while running.
-        //UpdatesWebsocket.sendLogMessageToClients(job, "ABORTED");
-        /*IRCClient.sendMessage(job.getUserName(), "Your job " + job.getId() + " was aborted.");
-        IRCClient.sendMessage("Logs URL: " + job.getLogsUrl());*/
-
+    public static void onJobAbort(Job job) {
         JobAbortEvent event = new JobAbortEvent(job);
         WikiBot.getBus().post(event);
-
-        /*TextChannel failChannel = WikiBot.getInstance().getTextChannelById(EnvConfig.getConfigs().get("discord_failure_channel"));
-        if (failChannel != null)
-            failChannel.sendMessage(String.format("%s for %s was aborted:\n\nThread: %s\nLogs: %s\nJob ID: `%s`\nFailed Task: %s\nExit code: `%s`\nExplanation: ```%s```", job.getName(), job.getUserName(), job.getThreadChannel(), job.getLogsUrl(), job.getId(), job.getRunningTask(), job.getFailedTaskCode(), job.getExplanation())).queue();*/
     }
 
     /**
@@ -86,9 +47,7 @@ public class JobEvents {
      *
      * @param job The job that was queued.
      */
-    public static void onJobQueued(Job job) { //This method is called when a job is queued, but before it starts running.
-        //UpdatesWebsocket.sendLogMessageToClients(job, "QUEUED");
-        //IRCClient.sendMessage(job.getUserName(), "Queued job! (" + job.getType() + "). You will be notified when it finishes. Use !status " + job.getId() + " for details.");
+    public static void onJobQueued(Job job) {
         JobQueuedEvent event = new JobQueuedEvent(job);
         WikiBot.getBus().post(event);
     }
