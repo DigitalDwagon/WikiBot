638056ba8dff3a0da2c6da645f2f1c6046899a13
DigitalDragon <34419970+DigitalDwagon@users.noreply.github.com>
2024-03-04 23:36:32 -0500

    WIP commit: cleanup wget-at lua hook

--- src/main/resources/mediawiki.lua
+++ src/main/resources/mediawiki.lua
@@ -1,8 +1,3 @@
---[[local urlparse = require("socket.url")
-local http = require("socket.http")
-local https = require("ssl.https")
-local cjson = require("cjson")
-local utf8 = require("utf8")]]
 local downloaded = {}
 local addedtolist = {}
 local seturls = {}
@@ -16,7 +11,6 @@ end
 
 
 wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
-    --print("dcp " .. urlpos["url"]["url"])
     local url = urlpos["url"]["url"]
     if processed(url) then
         return false
@@ -27,81 +21,9 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
     return verdict
 end
 
---[[wget.callbacks.get_urls = function(file, url, is_css, iri)
-    print("gu " .. url)
-    downloaded[url] = true
-    local urls = {}
-
-    if string.match(url, "https://archiveteam%.invalid/wikibot%-input%-file/") then
-        -- content after https://archiveteam.invalid/wikibot-input-file/ is the filename with the list of URLs
-        local filename = string.match(url, "https://archiveteam%.invalid/wikibot%-input%-file/(.*)")
-
-        local file = io.open(filename, "r")
-        if file then
-            print("Reading URLs from " .. filename)
-            for line in file:lines() do
-                table.insert(urls, { url=line })
-            end
-            file:close()
-        end
-        print("Returning URLs from " .. url)
-        return urls
-    end
-
-    --try to extract images, css, and javascript from the page
-    for url in file:gmatch('img.-src="([^"]+)"') do
-        if not processed(url) then
-            table.insert(urls, {url=url})
-            addedtolist[url] = true
-        end
-    end
-
-    for url in file:gmatch('link.-href="([^"]+)"') do
-        if not processed(url) then
-            table.insert(urls, {url=url})
-            addedtolist[url] = true
-        end
-    end
-
-    for url in file:gmatch('script.-src="([^"]+)"') do
-        if not processed(url) then
-            table.insert(urls, {url=url})
-            addedtolist[url] = true
-        end
-    end
-
-    return urls
-end--]]
-
 local url_count = 0
 
---wget.callbacks.write_to_warc = function(url, http_stat)
---    status_code = http_stat["statcode"]
---    print(url_count .. "=" .. status_code .. " " .. url["url"])
---    url_count = url_count + 1
---    downloaded[url["url"]] = true
---    return true
---end
-
 wget.callbacks.httploop_result = function(url, err, http_stat)
-    --[[if string.match(url["url"], "https://archiveteam%.invalid/wikibot%-input%-file/") then
-        -- content after https://archiveteam.invalid/wikibot-input-file/ is the filename with the list of URLs
-        local filename = string.match(url["url"], "https://archiveteam%.invalid/wikibot%-input%-file/(.*)")
-
-        local file = io.open(filename, "r")
-        if file then
-            print("Reading URLs from " .. filename)
-            for line in file:lines() do
-                seturls[line] = true
-            end
-            file:close()
-        end
-        print("Returning URLs from " .. url["url"])
-    end]]
-
-
-
-    --print("hr " .. url["url"])
     status_code = http_stat["statcode"]
     print(url_count .. "=" .. status_code .. " " .. url["url"])
     downloaded[url["url"]] = true
