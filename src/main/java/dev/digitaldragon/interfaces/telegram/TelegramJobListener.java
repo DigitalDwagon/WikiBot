c8339b3205461f224f9c80f5a394ab77fc3897f2
DigitalDragon <34419970+DigitalDwagon@users.noreply.github.com>
2024-04-25 18:57:07 -0400

    feat: use JobMeta in Telegram interfaces

--- src/main/java/dev/digitaldragon/interfaces/telegram/TelegramJobListener.java
+++ src/main/java/dev/digitaldragon/interfaces/telegram/TelegramJobListener.java
@@ -3,6 +3,7 @@ package dev.digitaldragon.interfaces.telegram;
 import dev.digitaldragon.WikiBot;
 import dev.digitaldragon.interfaces.api.UpdatesWebsocket;
 import dev.digitaldragon.jobs.Job;
+import dev.digitaldragon.jobs.JobMeta;
 import dev.digitaldragon.jobs.events.JobAbortEvent;
 import dev.digitaldragon.jobs.events.JobFailureEvent;
 import dev.digitaldragon.jobs.events.JobQueuedEvent;
@@ -14,48 +15,65 @@ public class TelegramJobListener {
     @EventHandler
     public void onJobSuccess(JobSuccessEvent event) {
         Job job = event.getJob();
+        JobMeta meta = job.getMeta();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(WikiBot.getConfig().getTelegramConfig().channelId());
-        sendMessage.setText(String.format("@%s Success! Job for %s completed.%n", job.getUserName(), job.getName()) +
-                "ID: " + job.getId() + "\n" +
-                "Archive URL: " + job.getArchiveUrl() + "\n" +
-                "Explanation: " + job.getExplanation() + "\n" +
-                "Logs: " + job.getLogsUrl());
+
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder.append(String.format("@%s Success! Job for %s completed.%n", meta.getUserName(), meta.getTargetUrl().orElse("unknown")))
+                .append("ID: ").append(job.getId()).append("\n")
+                .append("Archive URL: ").append(job.getArchiveUrl()).append("\n")
+                .append("Logs: ").append(job.getLogsUrl());
+        if (meta.getExplain().isPresent()) messageBuilder.append("\nExplanation: ").append(meta.getExplain().get());
+        sendMessage.setText(messageBuilder.toString());
+
         TelegramClient.getBot().tryToExecute(sendMessage);
     }
+
 
     @EventHandler
     public void onJobFailure(JobFailureEvent event) {
         Job job = event.getJob();
+        JobMeta meta = job.getMeta();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(WikiBot.getConfig().getTelegramConfig().channelId());
-        sendMessage.setText(String.format("@%s Job for %s failed with exit code %s.%n", job.getUserName(), job.getName(), job.getFailedTaskCode()) +
-                "ID: " + job.getId() + "\n" +
-                "Explanation: " + job.getExplanation() + "\n" +
-                "Logs: " + job.getLogsUrl());
+
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder.append(String.format("@%s Job for %s failed with exit code %s.%n", meta.getUserName(), meta.getTargetUrl().orElse("unknown"), job.getFailedTaskCode()))
+                .append("ID: ").append(job.getId()).append("\n")
+                .append("Logs: ").append(job.getLogsUrl()).append("\n");
+        if (meta.getExplain().isPresent()) messageBuilder.append("\nExplanation: ").append(meta.getExplain().get());
+        sendMessage.setText(messageBuilder.toString());
+
         TelegramClient.getBot().tryToExecute(sendMessage);
     }
 
     @EventHandler
     public void onJobAbort(JobAbortEvent event) {
         Job job = event.getJob();
+        JobMeta meta = job.getMeta();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(WikiBot.getConfig().getTelegramConfig().channelId());
-        sendMessage.setText(String.format("@%s Job for %s was aborted.%n", job.getUserName(), job.getName()) +
-                "ID: " + job.getId() + "\n" +
-                "Explanation: " + job.getExplanation() + "\n" +
-                "Logs: " + job.getLogsUrl());
+
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder.append(String.format("@%s Job for %s was aborted.%n", meta.getUserName(), meta.getTargetUrl().orElse("unknown")))
+                .append("ID: ").append(job.getId()).append("\n")
+                .append("Logs: ").append(job.getLogsUrl()).append("\n");
+        if (meta.getExplain().isPresent()) messageBuilder.append("\nExplanation: ").append(meta.getExplain().get());
+        sendMessage.setText(messageBuilder.toString());
+
         TelegramClient.getBot().tryToExecute(sendMessage);
     }
 
     @EventHandler
     public void onJobQueued(JobQueuedEvent event) {
         Job job = event.getJob();
+        JobMeta meta = job.getMeta();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(WikiBot.getConfig().getTelegramConfig().channelId());
         //        IRCClient.sendMessage(job.getUserName(), "Queued job! (" + job.getType() + "). You will be notified when it finishes. Use !status " + job.getId() + " for details.");
 
-        sendMessage.setText(String.format("@%s Job for %s queued!%nYou will be notified when it finishes. Use !status %s for details.", job.getUserName(), job.getName(), job.getId()));
+        sendMessage.setText(String.format("@%s Job for %s queued!%nYou will be notified when it finishes. Use !status %s for details.", meta.getUserName(), meta.getTargetUrl().orElse("unknown"), job.getId()));
         TelegramClient.getBot().tryToExecute(sendMessage);
     }
 }
