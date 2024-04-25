2a52f3b14dd1dc6bdcbcaedd790629b30afbb3e4
DigitalDragon <34419970+DigitalDwagon@users.noreply.github.com>
2024-04-25 18:35:50 -0400

    feat: use JobMeta in Dashboard

--- src/main/java/dev/digitaldragon/interfaces/api/Dashboard.java
+++ src/main/java/dev/digitaldragon/interfaces/api/Dashboard.java
@@ -2,6 +2,7 @@ package dev.digitaldragon.interfaces.api;
 
 import dev.digitaldragon.jobs.Job;
 import dev.digitaldragon.jobs.JobManager;
+import dev.digitaldragon.jobs.JobMeta;
 import lombok.SneakyThrows;
 
 import java.io.IOException;
@@ -62,14 +63,17 @@ public class Dashboard {
     private static String getCard(Job job) {
         StringBuilder sb = new StringBuilder();
         sb.append("<div class=\"job-container\">");
-        if (job.getName().startsWith("http://") || job.getName().startsWith("https://")) {
-            sb.append(String.format("<h3 class=\"job-url\"><a href=\"%s\">%s</a></h3>", job.getName(), job.getName()));
+        JobMeta meta = job.getMeta();
+
+        if (meta.getTargetUrl().isPresent()) {
+            String jobUrl = meta.getTargetUrl().get();
+            sb.append(String.format("<h3 class=\"job-url\"><a href=\"%s\">%s</a></h3>", jobUrl, jobUrl));
         } else {
-            sb.append(String.format("<h3 class=\"job-url\">%s</h3>", job.getName()));
+            sb.append(String.format("<h3 class=\"job-url\">%s</h3>", "Unknown URL"));
         }
-        sb.append(String.format("<p class=\"job-details\">(for %s - job %s)</p>", job.getUserName(), job.getId()));
+        sb.append(String.format("<p class=\"job-details\">(for %s - job %s)</p>", job.getMeta().getUserName(), job.getId()));
         sb.append(String.format("<p class=\"job-logs\" id=\"%s\">", job.getId()));
-        sb.append(job.getExplanation()).append("\n");
+        if (meta.getExplain().isPresent())  sb.append(meta.getExplain().get()).append("\n");
         sb.append("Logs will appear below as they are generated:" + "\n");
         sb.append("---- Web: Logs start ----" + "\n");
         sb.append("</p>");
