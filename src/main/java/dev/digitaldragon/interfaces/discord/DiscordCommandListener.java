f2981e2977d4e0f1693472fa7e2d1086916b9bd8
DigitalDragon <34419970+DigitalDwagon@users.noreply.github.com>
2024-04-26 21:10:56 -0400

    feat: revive Discord interface for all job types

--- src/main/java/dev/digitaldragon/interfaces/discord/DiscordCommandListener.java
+++ src/main/java/dev/digitaldragon/interfaces/discord/DiscordCommandListener.java
@@ -1,20 +1,25 @@
 package dev.digitaldragon.interfaces.discord;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
 import dev.digitaldragon.WikiBot;
 import dev.digitaldragon.interfaces.UserErrorException;
-import dev.digitaldragon.interfaces.generic.WikiTeam3Helper;
 import dev.digitaldragon.jobs.Job;
 import dev.digitaldragon.jobs.JobManager;
 import dev.digitaldragon.jobs.JobMeta;
+import dev.digitaldragon.jobs.dokuwiki.DokuWikiDumperArgs;
+import dev.digitaldragon.jobs.dokuwiki.DokuWikiDumperJob;
 import dev.digitaldragon.jobs.mediawiki.WikiTeam3Args;
 import dev.digitaldragon.jobs.mediawiki.WikiTeam3Job;
+import dev.digitaldragon.jobs.pukiwiki.PukiWikiDumperArgs;
+import dev.digitaldragon.jobs.pukiwiki.PukiWikiDumperJob;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,13 +28,70 @@ public class DiscordCommandListener extends ListenerAdapter {
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         System.out.println("got event");
         if (event.getName().equals("mediawiki_dump")) onMediaWikiSlashCommandInteraction(event);
+        if (event.getName().equals("dokuwiki_dump")) onDokuWikiSlashCommandInteraction(event);
+        if (event.getName().equals("pukiwiki_dump")) onPukiWikiSlashCommandInteraction(event);
+
     }
 
 
-    public void onMediaWikiSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+    public <T> T processArgs(T args, SlashCommandInteractionEvent event) {
+        try {
+            System.out.println(Arrays.toString(getArgsFromOptions(event)));
+            JCommander.newBuilder()
+                    .addObject(args)
+                    .build()
+                    .parse(getArgsFromOptions(event));
+            // Assuming args has a check() method
+            if (args instanceof WikiTeam3Args) ((WikiTeam3Args) args).check();
+            if (args instanceof DokuWikiDumperArgs) ((DokuWikiDumperArgs) args).check();
+            if (args instanceof PukiWikiDumperArgs) ((PukiWikiDumperArgs) args).check();
+        } catch (ParameterException e) {
+            event.reply("Invalid parameters or options! Double check your extra-args").setEphemeral(true).queue();
+            return null;
+        } catch (UserErrorException e) {
+            event.reply(e.getMessage()).setEphemeral(true).queue();
+            return null;
+        }
+        return args;
+    }
 
+    private void submitJob(SlashCommandInteractionEvent event, Job job) {
+        String jobId = UUID.randomUUID().toString();
+        job.getMeta().setPlatform(JobMeta.JobPlatform.DISCORD);
+        job.getMeta().setDiscordUserId(event.getUser().getId());
+        WikiBot.getDiscordClient().getJobListener().setJobChannel(jobId, event.getChannel());
+
+        JobManager.submit(job);
+        event.reply("Done! Running job `" + jobId + "`. I will notify you when it updates. Use `/status` with the job ID or use the `Status` button to manually check the job.")
+                .addEmbeds(DiscordClient.getStatusEmbed(job).build())
+                .addActionRow(DiscordClient.getJobActionRow(job))
+                .setEphemeral(true).queue();
+    }
+
+    public void onMediaWikiSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        WikiTeam3Args args = processArgs(new WikiTeam3Args(), event);
+        Job job = new WikiTeam3Job(event.getUser().getName(), UUID.randomUUID().toString(), args);
+        submitJob(event, job);
+    }
+
+    public void onDokuWikiSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        DokuWikiDumperArgs args = processArgs(new DokuWikiDumperArgs(), event);
+        Job job = new DokuWikiDumperJob(event.getUser().getName(), UUID.randomUUID().toString(), args.getUrl(), args.get(), args.getExplain());
+        submitJob(event, job);
+    }
+
+    public void onPukiWikiSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        PukiWikiDumperArgs args = processArgs(new PukiWikiDumperArgs(), event);
+        Job job = new PukiWikiDumperJob(event.getUser().getName(), UUID.randomUUID().toString(), args);
+        submitJob(event, job);
+    }
+
+
+    private String[] getArgsFromOptions(SlashCommandInteractionEvent event) {
+        System.out.println("getting args from options");
         List<String> opts = new ArrayList<>();
         for (var option : event.getOptions()) {
+            System.out.println(option.getName() + " " + option.getType() + " " + option.getAsString());
             String name = option.getName();
             if (name.equals("ignore-disposition-missing")) name = "ignore-disposition-header-missing";
 
@@ -52,34 +114,7 @@ public class DiscordCommandListener extends ListenerAdapter {
 
 
         }
-        System.out.println("opts: " + opts);
-
-        String[] unparsedArgs = opts.toArray(new String[0]);
-        WikiTeam3Args args = new WikiTeam3Args();
-        JCommander.newBuilder()
-                .addObject(args)
-                .build()
-                .parse(unparsedArgs);
-        try {
-            args.check();
-        } catch (UserErrorException e) {
-            event.reply(e.getMessage()).setEphemeral(true).queue();
-            return;
-        }
-
-        String jobId = UUID.randomUUID().toString();
-        Job job = new WikiTeam3Job(event.getUser().getName(), jobId, args);
-        job.getMeta().setPlatform(JobMeta.JobPlatform.DISCORD);
-        job.getMeta().setDiscordUserId(event.getUser().getId());
-        WikiBot.getDiscordClient().getJobListener().setJobChannel(jobId, event.getChannel());
-
-
-        JobManager.submit(job);
-        event.reply("Done! Running job `" + jobId + "`. I will notify you when it updates. Use `/status` with the job ID or use the `Status` button to manually check the job.")
-                .addEmbeds(DiscordClient.getStatusEmbed(job).build())
-                .addActionRow(DiscordClient.getJobActionRow(job))
-                .setEphemeral(true).queue();
-
+        return opts.toArray(new String[0]);
     }
 
 
