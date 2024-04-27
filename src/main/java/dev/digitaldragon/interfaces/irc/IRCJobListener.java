8684735d79d934e854d1bcf8c7cd938b03a84695
DigitalDragon <34419970+DigitalDwagon@users.noreply.github.com>
2024-04-26 21:51:28 -0400

    feat: don't send job updates to IRC unless a job was started there

--- src/main/java/dev/digitaldragon/interfaces/irc/IRCJobListener.java
+++ src/main/java/dev/digitaldragon/interfaces/irc/IRCJobListener.java
@@ -13,6 +13,8 @@ public class IRCJobListener {
     public void onJobSuccess(JobSuccessEvent event) {
         Job job = event.getJob();
         JobMeta meta = job.getMeta();
+        if (meta.getPlatform() != null && meta.getPlatform() != JobMeta.JobPlatform.IRC) return;
+
         IRCClient.sendMessage(meta.getUserName(), String.format("%sSuccess!%s Job for %s %s(%s)%s completed.", IRCFormat.LIGHT_GREEN, IRCFormat.RESET, meta.getTargetUrl().orElse("unknown"), IRCFormat.GREY, job.getId(), IRCFormat.RESET));
         IRCClient.sendMessage("Archive URL: " + job.getArchiveUrl());
         if (meta.getExplain().isPresent()) IRCClient.sendMessage("Explanation: " + meta.getExplain().get());
@@ -23,6 +25,7 @@ public class IRCJobListener {
     public void onJobFailure(JobFailureEvent event) {
         Job job = event.getJob();
         JobMeta meta = job.getMeta();
+        if (meta.getPlatform() != null && meta.getPlatform() != JobMeta.JobPlatform.IRC) return;
 
         if (job.getFailedTaskCode() == 88 && job.getArchiveUrl() != null) {
             IRCClient.sendMessage(meta.getUserName(), String.format("%s has %salready been archived%s in the past year! Use --force to override. %s(for %s)", meta.getTargetUrl().orElse("unknown"), IRCFormat.ORANGE, IRCFormat.RESET, IRCFormat.GREY, job.getId()));
@@ -40,6 +43,8 @@ public class IRCJobListener {
     public void onJobAbort(JobAbortEvent event) {
         Job job = event.getJob();
         JobMeta meta = job.getMeta();
+        if (meta.getPlatform() != null && meta.getPlatform() != JobMeta.JobPlatform.IRC) return;
+
         IRCClient.sendMessage(meta.getUserName(), String.format("Your job for %s %s(%s)%s was %saborted.", meta.getTargetUrl().orElse("unknown"), IRCFormat.GREY, job.getId(), IRCFormat.RESET, IRCFormat.ORANGE));
         if (meta.getExplain().isPresent()) IRCClient.sendMessage("Explanation: " + meta.getExplain().get());
         IRCClient.sendMessage("Logs URL: " + job.getLogsUrl());
@@ -49,6 +54,8 @@ public class IRCJobListener {
     public void onJobQueued(JobQueuedEvent event) {
         Job job = event.getJob();
         JobMeta meta = job.getMeta();
+        if (meta.getPlatform() != null && meta.getPlatform() != JobMeta.JobPlatform.IRC) return;
+
         IRCClient.sendMessage(meta.getUserName(), "Queued job! (" + job.getType() + "). You will be notified when it finishes. Use !status " + job.getId() + " for details.");
     }
 }
