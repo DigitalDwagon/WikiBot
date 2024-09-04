d377570102502bc861084ad6034b39230f266cf2
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2024-09-04 01:29:47 -0400

    feat(warc): send http Referer header for page embeds

--- src/main/resources/mediawiki.lua
+++ src/main/resources/mediawiki.lua
@@ -47,31 +47,10 @@ if resume_file ~= nil then
 	api_url = resume_data["api_url"]
 	index_url = resume_data["index_url"]
 	page_queue_start = resume_data["page_queue_start"]
+	urls_to_queue = resume_data["urls_to_queue"]
+	addedtolist = resume_data["addedtolist"]
+	downloaded = resume_data["downloaded"]
 	print("Loaded meta.json")
-end
-
-local resume_file = io.open("wikibot/queue.json", "r")
-if resume_file ~= nil then
-	local resume_content = resume_file:read("*all")
-	resume_file:close()
-	urls_to_queue = JSON:decode(resume_content)
-	print("Loaded queue.json")
-end
-
-resume_file = io.open("wikibot/addedtolist.json", "r")
-if resume_file ~= nil then
-	local resume_content = resume_file:read("*all")
-	resume_file:close()
-	addedtolist = JSON:decode(resume_content)
-	print("Loaded addedtolist.json")
-end
-
-resume_file = io.open("wikibot/downloaded.json", "r")
-if resume_file ~= nil then
-	local resume_content = resume_file:read("*all")
-	resume_file:close()
-	downloaded = JSON:decode(resume_content)
-	print("Loaded downloaded.json")
 end
 
 --
@@ -87,32 +66,44 @@ processed = function(url)
 end
 
 -- Queues a url
-queue_url = function(url)
-		table.insert(urls_to_queue, {url=url})
-		addedtolist[url] = true
+queue_url = function(url, referer)
+	local headers = {}
+	if referer ~= nil then
+		headers["Referer"] = referer
+	end
+	table.insert(urls_to_queue, {url=url, headers=headers})
+	addedtolist[url] = true
 	print("	-> " .. url)
 end
 
 -- Queues a url if it has not already been queued
-safe_queue_url = function (url)
+safe_queue_url = function (url, referer)
 	if not processed(url) then
-		queue_url(url)
+		queue_url(url, referer)
 	end
 end
 
 -- safe_queue_url but sets expect_html
-safe_queue_html = function(url)
+safe_queue_html = function(url, referer)
 	if not processed(url) then
-		table.insert(urls_to_queue, {url=url, link_expect_html=1})
+		local headers = {}
+		if referer ~= nil then
+			headers["Referer"] = referer
+		end
+		table.insert(urls_to_queue, {url=url, link_expect_html=1, headers=headers})
 		addedtolist[url] = true
 		print("	-> " .. url)
 	end
 end
 
 -- safe_queue_url but sets expect_css
-safe_queue_css = function(url)
+safe_queue_css = function(url, referer)
 	if not processed(url) then
-		table.insert(urls_to_queue, {url=url, link_expect_css=1})
+		local headers = {}
+		if referer ~= nil then
+			headers["Referer"] = referer
+		end
+		table.insert(urls_to_queue, {url=url, link_expect_css=1, headers=headers})
 		addedtolist[url] = true
 		print("	-> " .. url)
 	end
@@ -166,6 +157,7 @@ end
 -- For API navigation/queuing
 -- also contains the sleep between urls
 wget.callbacks.httploop_result = function(url, err, http_stat)
+	local fullurl = url["url"]
 	local sleep_time = delay
 	if retries > 0 then
 		sleep_time = retry_delay ^ retries
@@ -198,7 +190,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
 	if api_url ~= nil and not page_queue_start then
 		-- This kicks off the process of paginating through the allpages API if it hasn't started and we know the API url
 		--table.insert(urls_to_queue, { url = api_url .. "?action=query&meta=siteinfo&siprop=namespaces&format=json"})
-		safe_queue_url(api_url .. "?action=query&meta=siteinfo&siprop=namespaces&format=json")
+		safe_queue_url(api_url .. "?action=query&meta=siteinfo&siprop=namespaces&format=json", nil)
 		page_queue_start = true
 	end
 
@@ -211,7 +203,7 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
 	debug_print("url for check is " .. url["url"])
 	debug_print("api url after guard clause is " .. api_url)
 
-	if starts_with(url["url"], api_url) then
+	if starts_with(fullurl, api_url) then
 		--try to parse json
 		debug_print("trying to parse API json for url " .. url["url"])
 
@@ -223,16 +215,16 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
 			local namespaces = query["namespaces"]
 			if namespaces ~= nil then
 				for key, namespace in pairs(namespaces) do
-					safe_queue_url(api_url .. "?action=query&generator=allpages&format=json&gaplimit=50&prop=info&inprop=url&gapnamespace=" .. namespace["id"])
+					safe_queue_url(api_url .. "?action=query&generator=allpages&format=json&gaplimit=50&prop=info&inprop=url&gapnamespace=" .. namespace["id"], nil)
 				end
 			end
 			local pages = query["pages"]
 			if pages ~= nil then
 				for key, page in pairs(pages) do
-					safe_queue_html(page["fullurl"])
-					safe_queue_html(page["editurl"]:gsub("action=edit", "action=history"))
+					safe_queue_html(page["fullurl"], nil)
+					safe_queue_html(page["editurl"]:gsub("action=edit", "action=history"), page["fullurl"], nil)
 					--safe_queue_html(page["editurl"]:gsub("action=edit", "action=info"))
-					safe_queue_url(page["editurl"]:gsub("action=edit", "action=raw"))
+					safe_queue_url(page["editurl"]:gsub("action=edit", "action=raw"), page["fullurl"], nil)
 				end
 			end
 		end
@@ -240,37 +232,37 @@ wget.callbacks.httploop_result = function(url, err, http_stat)
 		local continue = json["continue"]
 		if continue ~= nil then
 			if continue["gapcontinue"] ~= nil then
-				local cleaned_url = url["url"]:gsub("&gapcontinue=[^&]*", "")
-				safe_queue_url(cleaned_url .. "&gapcontinue=" .. continue["gapcontinue"])
+				local cleaned_url = fullurl:gsub("&gapcontinue=[^&]*", "")
+				safe_queue_url(cleaned_url .. "&gapcontinue=" .. continue["gapcontinue"], nil)
 			end
 		end
 	end
 
 	for src in content:gmatch('<img[^>]+src="([^"]+)"') do
-		safe_queue_url(urlparse.absolute(url["url"], html_decode(src)))
+		safe_queue_url(urlparse.absolute(url["url"], html_decode(src)), fullurl)
 	end
 
 	for src in content:gmatch('<script[^>]+src="([^"]+)"') do
-		safe_queue_url(urlparse.absolute(url["url"], html_decode(src)))
+		safe_queue_url(urlparse.absolute(url["url"], html_decode(src)), fullurl)
 	end
 
 	for srcset in content:gmatch('<img[^>]+srcset="([^"]+)"') do
 		for srcurl in srcset:gmatch("([^ ]+) [^,]+,") do
-			safe_queue_url(urlparse.absolute(url["url"], html_decode(srcurl)))
+			safe_queue_url(urlparse.absolute(url["url"], html_decode(srcurl)), fullurl)
 		end
 	end
 
 	for href in content:gmatch('<link[^>]+rel="stylesheet"[^>]+href="([^"]+)"') do
 		debug_print("	-> " .. urlparse.absolute(url["url"], html_decode(href)))
-		safe_queue_url(urlparse.absolute(url["url"], html_decode(href)))
+		safe_queue_url(urlparse.absolute(url["url"], html_decode(href)), fullurl)
 	end
 
 	for href in content:gmatch('<a[^>]+href="([^"]+)"[^>]+class="[^"]*mw%-changeslist%-date[^"]*"[^>]+') do
-		safe_queue_html(urlparse.absolute(url["url"], html_decode(href)))
+		safe_queue_html(urlparse.absolute(url["url"], html_decode(href)), fullurl)
 	end
 
 	for href in content:gmatch('<a[^>]+href="([^"]+)"[^>]+class="[^"]*mw%-nextlink[^"]*"[^>]*') do
-		safe_queue_html(urlparse.absolute(url["url"], html_decode(href)))
+		safe_queue_html(urlparse.absolute(url["url"], html_decode(href)), fullurl)
 	end
 
 
@@ -283,7 +275,7 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
 
 	if parent["file"] == "load.php" then
 		debug_print("load.php dependency " .. urlpos["url"]["url"])
-		safe_queue_url(urlpos["url"]["url"])
+		safe_queue_url(urlpos["url"]["url"], parent["url"])
 	end
 
 	return false
@@ -321,6 +313,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
 	return {}
 end
 
+--note: (wget-at bug?) returning false still causes a request record to be written to the warc
 wget.callbacks.write_to_warc = function(url, http_stat)
 	debug_print(table.show({url, http_stat}, "write_to_warc"))
 	print(url_count .. " = " .. http_stat["statcode"] .. " " .. url["url"])
@@ -341,10 +334,10 @@ wget.callbacks.before_exit = function(exit_status, exit_status_string)
 	meta["api_url"] = api_url
 	meta["index_url"] = index_url
 	meta["page_queue_start"] = page_queue_start
+	meta["urls_to_queue"] = urls_to_queue
+	meta["addedtolist"] = addedtolist
+	meta["downloaded"] = downloaded
 	write_to_file("wikibot/meta.json", JSON:encode_pretty(meta))
-	write_to_file("wikibot/queue.json", JSON:encode_pretty(urls_to_queue))
-	write_to_file("wikibot/addedtolist.json", JSON:encode_pretty(addedtolist))
-	write_to_file("wikibot/downloaded.json", JSON:encode_pretty(downloaded))
 
 	if abort_grab then
 		print("wget failed, exiting...")
