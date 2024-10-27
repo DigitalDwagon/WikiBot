f203978a610cec81aee54463e38c1de29e3751ae
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2024-10-27 17:42:25 -0400

    release: bump 1.6.2

--- src/main/java/dev/digitaldragon/WikiBot.java
+++ src/main/java/dev/digitaldragon/WikiBot.java
@@ -36,7 +36,7 @@ public class WikiBot {
     @Getter
     private static LogFiles logFiles = new LogFiles();
     public static String getVersion() {
-        return "1.6.1";
+        return "1.6.2";
     }
 
     public static void main (String[] args) {
