53dec6be356417a26d1ba774cc621444e95ad73c
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2025-04-07 14:33:51 -0400

    release: bump 1.7.0

--- src/main/java/dev/digitaldragon/WikiBot.java
+++ src/main/java/dev/digitaldragon/WikiBot.java
@@ -51,7 +51,7 @@ public class WikiBot {
             .registerTypeAdapter(Optional.class, new OptionalSerializer<>())
             .create();
     public static String getVersion() {
-        return "1.6.4";
+        return "1.7.0";
     }
 
     @Getter
