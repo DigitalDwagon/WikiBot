7be53c2ea337e705830f62b993fffaa839127cf7
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2025-07-18 21:35:20 -0400

    chore: remove live discord logging

--- src/main/java/dev/digitaldragon/interfaces/discord/DiscordJobListener.java
+++ src/main/java/dev/digitaldragon/interfaces/discord/DiscordJobListener.java
@@ -3,21 +3,20 @@ package dev.digitaldragon.interfaces.discord;
 import dev.digitaldragon.WikiBot;
 import dev.digitaldragon.jobs.Job;
 import dev.digitaldragon.jobs.JobMeta;
-import dev.digitaldragon.jobs.events.*;
+import dev.digitaldragon.jobs.events.JobAbortEvent;
+import dev.digitaldragon.jobs.events.JobFailureEvent;
+import dev.digitaldragon.jobs.events.JobQueuedEvent;
+import dev.digitaldragon.jobs.events.JobSuccessEvent;
 import net.badbird5907.lightning.annotation.EventHandler;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
 public class DiscordJobListener {
-    private static Map<String, ThreadChannel> jobChannels = new HashMap<>();
-    private static Map<String, String> jobLogs = new HashMap<>();
     private static final Map<String, MessageChannel> jobUpdateChannels = new HashMap<>();
 
     public void setJobChannel(String jobId, MessageChannel channel) {
@@ -49,14 +48,6 @@ public class DiscordJobListener {
             default -> "";
         };
 
-        sendLogs(jobLogs.getOrDefault(job.getId(), ""), job);
-
-        jobChannels.get(job.getId())
-                .sendMessage(message)
-                .setActionRow(DiscordClient.getJobActionRow(job))
-                .setEmbeds(DiscordClient.getStatusEmbed(job).build())
-                .queue();
-
         List<MessageChannel> channels = new ArrayList<>();
 
         if (meta.getDiscordUserId().isPresent()) {
@@ -75,8 +66,6 @@ public class DiscordJobListener {
             System.out.println("Sending to " + jobUpdateChannels.get(job.getId()).getId());
             channels.add(jobUpdateChannels.get(job.getId()));
         }
-
-
 
         for (MessageChannel channel : channels) {
             channel.sendMessage(message)
@@ -110,51 +99,6 @@ public class DiscordJobListener {
         if (channel == null) {
             LoggerFactory.getLogger(DiscordJobListener.class).error("Failed to access Discord channel " + WikiBot.getConfig().getDiscordConfig().channelId());
             return;
-        }
-
-        channel.createThreadChannel(truncateString(job.getMeta().getTargetUrl().orElse(job.getId()), 100))
-                .setAutoArchiveDuration(ThreadChannel.AutoArchiveDuration.TIME_1_HOUR)
-                .queue(thread -> {
-                    thread.sendMessageEmbeds(DiscordClient.getStatusEmbed(job).build())
-                            .setActionRow(DiscordClient.getJobActionRow(job))
-                            .queue(sentMessage -> sentMessage.pin().queue());
-                    jobChannels.put(job.getId(), thread);
-                });
-
-    }
-
-    @EventHandler
-    public synchronized void onJobLog(JobLogEvent event) {
-        Job job = event.getJob();
-        String message = event.getMessage();
-        String logs = jobLogs.getOrDefault(job.getId(), "");
-        if (logs.length() + message.length() > Message.MAX_CONTENT_LENGTH - 6) {
-            sendLogs(logs, job);
-            logs = jobLogs.getOrDefault(job.getId(), "");
-        }
-        message = truncateString(message, Message.MAX_CONTENT_LENGTH - 6);
-        logs += message + "\n";
-        jobLogs.put(job.getId(), logs);
-    }
-
-    private synchronized void sendLogs(String message, Job job) {
-        if (message.isEmpty()) return;
-        message = truncateString(message, Message.MAX_CONTENT_LENGTH - 6);
-        try {
-            jobChannels.get(job.getId()).sendMessage("```" + message + "```").queue();
-            jobLogs.put(job.getId(), "");
-        } catch (Exception e) {
-            LoggerFactory.getLogger(DiscordJobListener.class).error("Failed to send logs to Discord channel for job " + job.getId(), e);
-        }
-
-    }
-
-
-    private static String truncateString(String message, int maxLength) {
-        if (message.length() <= maxLength) {
-            return message;
-        } else {
-            return message.substring(0, maxLength - 3) + "...";
         }
     }
 }
