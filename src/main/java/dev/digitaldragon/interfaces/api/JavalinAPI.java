26ccb704c290351223cf142bcd0105b605cb5148
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2026-01-31 15:52:16 -0500

    feat: add ?details=true param to the job list API
    allows you to get the full job details (like /api/jobs/JOBID) for all jobs at once

--- src/main/java/dev/digitaldragon/interfaces/api/JavalinAPI.java
+++ src/main/java/dev/digitaldragon/interfaces/api/JavalinAPI.java
@@ -5,9 +5,12 @@ import dev.digitaldragon.jobs.Job;
 import dev.digitaldragon.jobs.JobManager;
 import io.javalin.Javalin;
 import jakarta.servlet.http.HttpServletRequest;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class JavalinAPI {
@@ -106,19 +109,18 @@ public class JavalinAPI {
 
     private static void getAllJobs() {
         app.get("/api/jobs", (ctx) -> {
-            JSONObject jsonObject = new JSONObject();
-            JSONArray runningJobs = new JSONArray();
-            for (Job job : JobManager.getRunningJobs()) {
-                runningJobs.put(job.getId());
+            if (ctx.queryParam("details") != null && ctx.queryParam("details").equalsIgnoreCase("true")) {
+                Map<String, List<Job>> response = new HashMap<>();
+                response.put("queued", JobManager.getQueuedJobs());
+                response.put("running", JobManager.getRunningJobs());
+                ctx.result(WikiBot.getGson().toJson(response));
+                return;
             }
-            jsonObject.put("running", runningJobs);
 
-            JSONArray queuedJobs = new JSONArray();
-            for (Job job : JobManager.getQueuedJobs()) {
-                queuedJobs.put(job.getId());
-            }
-            jsonObject.put("queued", queuedJobs);
-            ctx.result(jsonObject.toString());
+            Map<String, List<String>> response = new HashMap<>();
+            response.put("queued", JobManager.getQueuedJobs().stream().map(Job::getId).toList());
+            response.put("running", JobManager.getRunningJobs().stream().map(Job::getId).toList());
+            ctx.result(WikiBot.getGson().toJson(response));
         });
     }
 
@@ -133,52 +135,6 @@ public class JavalinAPI {
             }
         });
     }
-
-    /*private static void createJob() {
-        app.post("/api/jobs", (ctx) -> {
-            try {
-                JSONObject json = new JSONObject(ctx.body());
-                if (!json.has("jobType")) {
-                    ctx.status(400).result(error("You must include a valid \"jobType\" in your request. It may be one of: WIKITEAM3"));
-                }
-                JSONObject clean = new JSONObject(ctx.body());
-                clean.remove("jobType");
-                String username = ctx.req().getHeader("X-Platform-User");
-
-                if (json.getString("jobType").equalsIgnoreCase("wikiteam3")) {
-                    System.out.println(clean);
-                    WikiTeam3Args args = new Gson().fromJson(clean.toString(), WikiTeam3Args.class);
-                    JobMeta meta = new JobMeta(username);
-                    meta.setExplain(args.getExplain());
-                    meta.setPlatform(JobMeta.JobPlatform.API);
-                    System.out.println(args.getExplain());
-                    System.out.println(args.getUrl());
-                    String jobId = UUID.randomUUID().toString();
-                    try {
-                        JobManager.submit(
-                                new WikiTeam3Job(args, meta, jobId)
-                        );
-                    } catch (JobLaunchException e) {
-                        ctx.status(400).result(error(e.getMessage()));
-                        return;
-                    }
-                    JSONObject response = new JSONObject();
-                    response.put("jobId", jobId);
-                    response.put("success", true);
-                    ctx.result(response.toString());
-                    return;
-                } else {
-                    ctx.status(400).result(error("Invalid job type"));
-                    return;
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            ctx.status(500).result("oops");
-        });
-    }*/
-
 
     private static String error(String message) {
         return new JSONObject().put("error", message).put("success", false).toString();
