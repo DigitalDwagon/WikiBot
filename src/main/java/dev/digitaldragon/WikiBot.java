7d484a5036ef7e523327d8b73f8bb44a7bbe0fd6
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2026-05-11 08:16:45 -0400

    release: bump 1.9.3

--- src/main/java/dev/digitaldragon/WikiBot.java
+++ src/main/java/dev/digitaldragon/WikiBot.java
@@ -51,7 +51,7 @@ public class WikiBot {
             .registerTypeAdapter(Optional.class, new OptionalSerializer<>())
             .create();
     public static String getVersion() {
-        return "1.9.2";
+        return "1.9.3";
     }
 
     @Getter
