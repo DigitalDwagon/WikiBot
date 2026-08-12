a6948745a25f063973e2f006a5ed5286de2b5ece
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2026-08-12 01:27:57 -0400

    cleanup: delete hanging SimpleLogsHandler and StringLogHandler classes
    obsoleted by ffc06cbd473f5c9ebd66f562fd1b4910c20c8839 - dropping GenericLogsHandler also caused these classes to become unused

--- src/main/java/dev/digitaldragon/jobs/StringLogHandler.java
+++
@@ -1,8 +0,0 @@
-package dev.digitaldragon.jobs;
-
-/**
- * This interface represents a handler for processing log messages as strings.
- */
-public interface StringLogHandler {
-    void onMessage(String message);
-}
